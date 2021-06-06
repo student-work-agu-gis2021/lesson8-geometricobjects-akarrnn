c454667b02bc18457cb1bbde37e6d561079be984
aaakbw <>
2021-06-06 19:34:30 +0000

    I wrote YOUR CODE HERE 9.

--- Exercise_1_problem_1_2.py
+++ Exercise_1_problem_1_2.py
@@ -176,6 +176,12 @@ except Exception as e:
 
 
 #  YOUR CODE HERE 9 to define get_length()
+def get_length(geom):
+  """geom calculate length"""
+  if geom.geom_type=='LineString':
+      return geom.length
+  elif geom.geom_type=='Polygon':
+     return geom.exterior.length
 
 # Test and demonstrate the usage of the function:
 
