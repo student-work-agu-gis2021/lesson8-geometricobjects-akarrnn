0b9e540f9f354557eab394404df65f188cb0a299
aaakbw <>
2021-06-06 20:01:06 +0000

    I wrote YOUR CODE HERE 6.

--- Exercise_1_problem_3_4.py
+++ Exercise_1_problem_3_4.py
@@ -124,6 +124,9 @@ print('lines length:', len(lines))
 # YOUR CODE HERE 6 to append LineString to lines
 #raise NotImplementedError()
 from shapely.geometry import LineString
+for orig,dest in zip(orig_points,dest_points):
+  line=LineString([data.orig,data.dest])
+  lines.append(line)
 
 # CODE FOR TESTING YOUR SOLUTION
 
